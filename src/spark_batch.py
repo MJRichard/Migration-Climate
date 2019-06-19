2206ddfb9da58693c1622f8ae81563b2090f6943
MJRichard <Mr.Martin.J.Richard@gmail.com>
2019-06-19 18:36:05 -0400

    Joining stations and observations

--- src/spark_batch.py
+++ src/spark_batch.py
@@ -13,6 +13,8 @@ Write result to PostgreSQL
 from pyspark import SparkConf, SparkContext, SQLContext
 from pyspark.sql import SparkSession, types
 from pyspark.sql.types import StructType, StructField, StringType, DateType, TimestampType, FloatType
+from pyspark.sql.functions import udf, struct
+from math import radians, sin, cos, sqrt, asin
 import math
 
 #attempts to get spark config working
@@ -38,13 +40,15 @@ HCN/CRN FLAG 77-79   Character
 WMO ID       81-85   Character
 '''
 
-ghcnd_raw = spark.read.text("../../Data/ghcnd-stations.txt")
-ghcnd_raw.select(
+ghcnd_raw = spark.read.text("../../Data/ghcnd-stations.txt").limit(1000)
+ghcnd_df=ghcnd_raw.select(
     ghcnd_raw.value.substr(1,11).alias('id'),
     ghcnd_raw.value.substr(13,8).cast('float').alias('Latitude'),
     ghcnd_raw.value.substr(23,8).cast('float').alias('Longitude'),
     ghcnd_raw.value.substr(32,6).cast('float').alias('Elevation')
-).show()
+)
+
+ghcnd_df.show()
 #float drops precision 0s
 
 '''
@@ -72,7 +76,7 @@ ghcnd_obs_schema = StructType([
 #raw data for obs_time in subset data in format 070.0 instead of 0700 due to read/write issue when subsetting
 
 weather_data = spark.read.csv("../../Data/2002subset.csv", schema=ghcnd_obs_schema, dateFormat='yyyyMMdd')
-weather_data.show()
+#weather_data.show()
 
 #change date to unified format
 
@@ -80,7 +84,7 @@ pidgeon_schema = StructType([
     StructField("eventid", StringType(), False),
     StructField("visible", StringType(),False),
     StructField("timestamp",TimestampType(),False),
-    StructField("longtitude",FloatType(),False),
+    StructField("longitude",FloatType(),False),
     StructField("latitude",FloatType(),False),
     StructField("gps",StringType(),True),
     StructField("ground_speed",StringType(),True),
@@ -93,11 +97,45 @@ pidgeon_schema = StructType([
     StructField("study_name",StringType(),True)])
 
 pidgeon_obs = spark.read.csv("../../Data/Pigeonsubset.csv", schema=pidgeon_schema, timestampFormat='yyyy-MM-dd HH:mm:ss.SSS',
-                             header=True)
+                             header=True).limit(100)
 pidgeon_obs = pidgeon_obs.withColumn('date', pidgeon_obs['timestamp'].cast('date'))
-pidgeon_obs.show()
+#pidgeon_obs.show()
 #timestamp text
-pidgeon_obs.printSchema()
+#pidgeon_obs.printSchema()
 
-#join data
+#join stations and observations to then calculate distance between every station and observation
+station_obs_join = pidgeon_obs.select("eventid", "latitude", "longitude").withColumnRenamed("latitude","obs_lat").withColumnRenamed("longitude","obs_long")\
+    .crossJoin(ghcnd_df.select("id","Latitude","Longitude").withColumnRenamed("Latitude","station_lat").withColumnRenamed("Longitude","station_long"))\
+    .select("eventid","id","obs_lat","obs_long","station_lat","station_long")
+
+#station_obs_join.count()
+
+def haversine_distance(lat1,lon1,lat2,lon2):
+    '''
+    Calculates Haversine distance between two lat/lon coordinates
+    array in format [lat1, lon1, lat2, lon2]
+    :param lat1: Latitude of first point
+    :param lon1: Longitude of first point
+    :param lat2: Latitude of second point
+    :param lon2: Longitude of second point
+    :returns:    Float, distance between two points in km
+    '''
+    R = 6372.8 # Earth radius in kilometers
+
+    delta_lat = radians(lat2 - lat1)
+    delta_lon = radians(lon2 - lon1)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    a = sin(delta_lat / 2.0) ** 2 + cos(lat1) * cos(lat2) * sin(delta_lon / 2.0) ** 2
+    c = 2 * asin(sqrt(a))
+
+    return R * c
+
+udf_func = udf(lambda a,b,c,d: haversine_distance(a,b,c,d),returnType=FloatType())
+
+station_obs_calc = station_obs_join.withColumn('dist',udf_func(station_obs_join['obs_lat'],station_obs_join['obs_long'],station_obs_join['station_lat'],station_obs_join['station_long']))
+
+#station_obs_calc=station_obs_join.withColumn("dist", haversine_distance("obs_lat", "obs_long", "station_lat", "station_long"))
+station_obs_calc.show()
 #calculate haversine distance
