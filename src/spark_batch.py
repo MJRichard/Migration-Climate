50643f6af2ca12061a6b75f083938a5fd9825b5c
MJRichard <Mr.Martin.J.Richard@gmail.com>
2019-07-08 05:35:10 -0400

    Clean up spark processing

--- src/spark_batch.py
+++ src/spark_batch.py
@@ -1,35 +1,26 @@
-'''
-Batch process in spark to join animal migration data with daily climate summary.
+"""
+Spark batch process in spark to join animal migration data with daily climate summary.
 
 Data:
 Climate summary for weather station id (gzipped)
 Weather station ids and locations
 Study data
 
-Join data based on same day, filter to nearest location
+Read in data, transform weather data to one row per station/day
 Write result to PostgreSQL
-'''
+"""
 
-from pyspark import SparkConf, SparkContext, SQLContext
 from pyspark.sql import SparkSession, types, DataFrameReader
 from pyspark.sql.types import StructType, StructField, StringType, DateType, TimestampType, FloatType, IntegerType
-from pyspark.sql.functions import udf, struct, col
-from math import radians, sin, cos, sqrt, asin
-import math
-import datetime
-
-#attempts to get spark config working
-#sqlContext = SQLContext(sc)
-#spark_conf = SparkConf().setAppName("WeatherStations")
-
-#sqlContext = SQLContext(sc)
 
 spark = SparkSession.builder.appName("Migration").getOrCreate()
 assert isinstance(spark.sparkContext, object)
-#sc = spark.sparkContext
 
+
+# read in each file, define schema to raise error if file format is different
 '''
-ghcnd-stations.txt format
+read in stations datafile
+ghcnd-stations.txt schema
 ID            1-11   Character
 LATITUDE     13-20   Real (decimal degrees)
 LONGITUDE    22-30   Real (decimal degrees)
@@ -41,25 +32,18 @@ HCN/CRN FLAG 77-79   Character
 WMO ID       81-85   Character
 '''
 
-#print("*************\n ************\n **********\n start  time: "+ datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")+"\n************\n ***************\n *************" )
+def read_stations(s3_location):
+    ghcnd_raw = spark.read.text(s3_location)
+    ghcnd_stations = ghcnd_raw.select(
+        ghcnd_raw.value.substr(1, 11).alias('id'),
+        ghcnd_raw.value.substr(13, 8).cast('float').alias('Latitude'),
+        ghcnd_raw.value.substr(23, 8).cast('float').alias('Longitude'),
+        ghcnd_raw.value.substr(32, 6).cast('float').alias('Elevation')
+    )
+    return ghcnd_stations
+# other columns not needed, so not processing
 
 
-#s3 bucket http://noaa-ghcn-pds.s3.amazonaws.com/ghcnd-stations.txt
-#ghcnd_raw = spark.read.text("../../Data/ghcnd-stations.txt").limit(1000)
-ghcnd_raw = spark.read.text("s3a://noaa-ghcn-pds/ghcnd-stations.txt")
-
-ghcnd_df=ghcnd_raw.select(
-    ghcnd_raw.value.substr(1,11).alias('id'),
-    ghcnd_raw.value.substr(13,8).cast('float').alias('Latitude'),
-    ghcnd_raw.value.substr(23,8).cast('float').alias('Longitude'),
-    ghcnd_raw.value.substr(32,6).cast('float').alias('Elevation')
-)
-#other columns not needed, so not processing
-
-#ghcnd_df.show()
-#float drops precision 0s
-
-#print("*************\n ************\n **********\n ghcnd read in" +str(ghcnd_df.count()) + " time: "+ datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")+"\n************\n ***************\n *************" )
 '''
 Daily data sets
     ID = 11 character station identification code. Please see ghcnd-stations section below for an explantation
@@ -72,33 +56,64 @@ Daily data sets
     OBS-TIME = 4-character time of observation in hour-minute format (i.e. 0700 =7:00 am)
 '''
 
-#define schema since there is no header
-ghcnd_obs_schema = StructType([
-    StructField("id", StringType(), False),
-    StructField("date", DateType(),False),
-    StructField("element",StringType(),False),
-    StructField("element_val",StringType(),False),
-    StructField("m_flag",StringType(),True),
-    StructField("q_flag",StringType(),True),
-    StructField("s_flag",StringType(),True),
-    StructField("obs_time",StringType(),True)])
-#raw data for obs_time in subset data in format 070.0 instead of 0700 due to read/write issue when subsetting
+def read_weather_obs(weather_path):
+    ghcnd_obs_schema = StructType([
+        StructField("id", StringType(), False),
+        StructField("date", DateType(), False),
+        StructField("element", StringType(), False),
+        StructField("element_val", StringType(), False),
+        StructField("m_flag", StringType(), True),
+        StructField("q_flag", StringType(), True),
+        StructField("s_flag", StringType(), True),
+        StructField("obs_time", StringType(), True)])
+    weather_data = spark.read.csv(weather_path, schema=ghcnd_obs_schema, dateFormat='yyyyMMdd') \
+        .select("id", "date", "element", "element_val")
+    return weather_data
 
 
-#Read in weather observations
-# AWS Bucket http://noaa-ghcn-pds.s3.amazonaws.com/csv.gz/1788.csv.gz
-#noaa-ghcn-pds.s3.amazonaws.com/csv.gz/1788.csv.gz
-#weather_data = spark.read.csv("../../Data/2002subset.csv", schema=ghcnd_obs_schema, dateFormat='yyyyMMdd')
+def read_pigeon_obs(pigeon_s3):
+    pigeon_schema = StructType([
+        StructField("eventid", StringType(), False),
+        StructField("visible", StringType(), False),
+        StructField("timestamp", TimestampType(), False),
+        StructField("longitude", FloatType(), False),
+        StructField("latitude", FloatType(), False),
+        StructField("gps", IntegerType(), True),
+        StructField("ground_speed", FloatType(), True),
+        StructField("height_above_sealevel", FloatType(), True),
+        StructField("outlier_flag", StringType(), True),
+        StructField("sensor_type", StringType(), True),
+        StructField("taxon_name", StringType(), True),
+        StructField("tag_local_identifier", StringType(), True),
+        StructField("individual_local_identifier", StringType(), True),
+        StructField("study_name", StringType(), True)])
+    pigeon_obs = spark.read.csv(pigeon_s3, schema=pigeon_schema,
+                                 timestampFormat='yyyy-MM-dd HH:mm:ss.SSS',
+                                 header=True)
+    return pigeon_obs
 
-weather_data = spark.read.csv("s3a://noaa-ghcn-pds/csv/2002.csv", schema=ghcnd_obs_schema, dateFormat='yyyyMMdd')\
-                    .select("id", "date", "element", "element_val")
-#weather_data.show()
 
-tmin = weather_data.filter(weather_data.element == "TMIN").withColumnRenamed("element_val", "tmin")
-tmax = weather_data.filter(weather_data.element == "TMAX").withColumnRenamed("element_val", "tmax")
-prcp = weather_data.filter(weather_data.element == "PRCP").withColumnRenamed("element_val", "prcp")
-snow = weather_data.filter(weather_data.element == "SNOW").withColumnRenamed("element_val", "snow")
-snwd = weather_data.filter(weather_data.element == "SNWD").withColumnRenamed("element_val", "snwd")
+# best practice is to move db location to config file, not in github directly
+def write_to_db(df, table_name):
+    db_url = 'jdbc:postgresql://migrationplus2.cbyji2jivihq.us-east-1.rds.amazonaws.com:5432/migrationplus'
+    propertiesval = {'user': 'migrationplus', 'password': 'migrationplus', 'batchsize': '50000'}
+    df.write.jdbc(url=db_url, table=table_name, mode='overwrite', properties=propertiesval)
+    return
+
+
+# read in all data and run
+ghcnd_stations = read_stations("s3a://noaa-ghcn-pds/ghcnd-stations.txt")
+pigeon_obs = read_pigeon_obs("s3a://insightmovementweather/MigrationData/Pigeon.csv")
+
+
+weather_df = read_weather_obs("s3a://noaa-ghcn-pds/csv/*.csv")
+
+# transform weather observations into one row that includes the 5 main features
+tmin = weather_df.filter(weather_df.element == "TMIN").withColumnRenamed("element_val", "tmin")
+tmax = weather_df.filter(weather_df.element == "TMAX").withColumnRenamed("element_val", "tmax")
+prcp = weather_df.filter(weather_df.element == "PRCP").withColumnRenamed("element_val", "prcp")
+snow = weather_df.filter(weather_df.element == "SNOW").withColumnRenamed("element_val", "snow")
+snwd = weather_df.filter(weather_df.element == "SNWD").withColumnRenamed("element_val", "snwd")
 
 tmin.createOrReplaceTempView("tmin")
 tmax.createOrReplaceTempView("tmax")
@@ -106,7 +121,8 @@ prcp.createOrReplaceTempView("prcp")
 snow.createOrReplaceTempView("snow")
 snwd.createOrReplaceTempView("snwd")
 
-flat_ghcnd2 = spark.sql("""Select COALESCE(tmin.id, tmax.id, prcp.id, snow.id, snwd.id) as id,
+# join for one observation per row
+flat_ghcnd = spark.sql("""Select COALESCE(tmin.id, tmax.id, prcp.id, snow.id, snwd.id) as id,
                   COALESCE(tmin.date, tmax.date, prcp.date,
                           snow.date, snwd.date) as date,
                   tmin.tmin, tmax.tmax, prcp.prcp, snow.snow, snwd.snwd
@@ -123,104 +139,10 @@ flat_ghcnd2 = spark.sql("""Select COALESCE(tmin.id, tmax.id, prcp.id, snow.id, s
                   FULL OUTER JOIN snwd
                   ON snwd.id=COALESCE(tmin.id, tmax.id, prcp.id, snow.id)
                    AND snwd.date=COALESCE(tmin.date, tmax.date,
-                   prcp.date, snow.date)""")
-#print("*************\n ************\n **********\n 2002 weather read in done" +str(weather_data.count()) + " time: "+ datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")+"\n************\n ***************\n *************" )
+                   prcp.date, snow.date) """)
 
-
-#change date to unified format
-
-pidgeon_schema = StructType([
-    StructField("eventid", StringType(), False),
-    StructField("visible", StringType(), False),
-    StructField("timestamp", TimestampType(), False),
-    StructField("longitude", FloatType(), False),
-    StructField("latitude", FloatType(), False),
-    StructField("gps", IntegerType(), True),
-    StructField("ground_speed", FloatType(), True),
-    StructField("height_above_sealevel", FloatType(), True),
-    StructField("outlier_flag", StringType(), True),
-    StructField("sensor_type", StringType(), True),
-    StructField("taxon_name", StringType(), True),
-    StructField("tag_local_identifier", StringType(), True),
-    StructField("individual_local_identifier", StringType(), True),
-    StructField("study_name", StringType(), True)])
-
-#pidgeon_obs = spark.read.csv("../../Data/Pigeonsubset.csv", schema=pidgeon_schema, timestampFormat='yyyy-MM-dd HH:mm:ss.SSS',
-#                             header=True).limit(100)
-pidgeon_obs = spark.read.csv("s3a://insightmovementweather/MigrationData/Pigeon.csv", schema=pidgeon_schema, timestampFormat='yyyy-MM-dd HH:mm:ss.SSS',
-                             header=True)
-
-
-#print("*************\n ************\n **********\n pidgeon read in done" +str(pidgeon_obs.count()) + " time: "+ datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")+"\n************\n ***************\n *************" )
-
-
-#pidgeon_obs = pidgeon_obs.withColumn('date', pidgeon_obs['timestamp'].cast('date'))
-#pidgeon_obs.show()
-#pidgeon_obs.printSchema()
-
-#join stations and observations to then calculate distance between every station and observation
-
-'''
-station_obs_join = pidgeon_obs.select("eventid", "latitude", "longitude").withColumnRenamed("latitude","obs_lat").withColumnRenamed("longitude","obs_long")\
-    .crossJoin(ghcnd_df.select("id","Latitude","Longitude").withColumnRenamed("Latitude","station_lat").withColumnRenamed("Longitude","station_long"))\
-    .select("eventid","id","obs_lat","obs_long","station_lat","station_long")
-
-#station_obs_join.count()
-'''
-
-def haversine_distance(lat1,lon1,lat2,lon2):
-    '''
-    Calculates Haversine distance between two lat/lon coordinates
-    array in format [lat1, lon1, lat2, lon2]
-    :param lat1: Latitude of first point
-    :param lon1: Longitude of first point
-    :param lat2: Latitude of second point
-    :param lon2: Longitude of second point
-    :returns:    Float, distance between two points in km
-    '''
-    R = 6372.8 # Earth radius in kilometers
-
-    delta_lat = radians(lat2 - lat1)
-    delta_lon = radians(lon2 - lon1)
-    lat1 = radians(lat1)
-    lat2 = radians(lat2)
-
-    a = sin(delta_lat / 2.0) ** 2 + cos(lat1) * cos(lat2) * sin(delta_lon / 2.0) ** 2
-    c = 2 * asin(sqrt(a))
-
-    return R * c
-
-udf_func = udf(lambda a,b,c,d: haversine_distance(a,b,c,d),returnType=FloatType())
-
-
-'''
-station_obs_calc = station_obs_join.withColumn('dist',udf_func(station_obs_join['obs_lat'],station_obs_join['obs_long'],station_obs_join['station_lat'],station_obs_join['station_long']))
-'''
-
-#station_obs_calc=station_obs_join.withColumn("dist", haversine_distance("obs_lat", "obs_long", "station_lat", "station_long"))
-#station_obs_calc.show()
-#calculate haversine distance
-#station_output=station_obs_calc.select(col("eventid"),col("id"), col("dist").alias("distance"))
-
-#station_obs_calc.write.csv("s3a://insightmovementweather/output_data/testjoin.csv")
-
-urlval = 'jdbc:postgresql://ec2-34-195-21-119.compute-1.amazonaws.com:5432/migrationplus'
-urlval2 = 'jdbc:postgresql://migrationplus2.cbyji2jivihq.us-east-1.rds.amazonaws.com:5432/migrationplus'
-
-propertiesval = {'user': 'migrationplus', 'password': 'migrationplus', 'batchsize': '50000'}
-#station_output.write.jdbc(url=urlval2, table='sensor_station_distance', mode='append', properties=propertiesval)
-
-pidgeon_obs.write.jdbc(url=urlval2, table='pidgeon_sensor', mode='overwrite', properties=propertiesval)
-
-#print("*************\n ************\n **********\n pidgeon write in??" +str(ghcnd_df.count()) + " time: "+ datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")+"\n************\n ***************\n *************" )
-
-#print(type(weather_data))
-#print("COUNT", weather_data.count())
-#weather_data.write.jdbc(url=urlval2, table='station_obs', mode='overwrite', properties=propertiesval)
-
-flat_ghcnd2.write.jdbc(url=urlval2, table='station_flatobs', mode='overwrite', properties=propertiesval)
-#print("*************\n ************\n **********\nweather obs write in??" +str(ghcnd_df.count()) + " time: "+ datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")+"\n************\n ***************\n *************" )
-
-
-ghcnd_df.write.jdbc(url=urlval2, table='stations', mode='overwrite', properties=propertiesval)
+#write all 3 to database
+write_to_db(pigeon_obs, 'pidgeon_sensor')
+write_to_db(flat_ghcnd, 'station_flatobs')
+write_to_db(ghcnd_stations, 'stations')
 
